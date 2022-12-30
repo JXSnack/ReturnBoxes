d97cced7c35ddfecd857dddc5f6d2e325f6a7cd5
JXSnack <jx.snack@gmail.com>
2022-12-30 13:49:06 +0100

    Init for chrome?

--- test.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('[style]')
-    .forEach(el => el.removeAttribute('style'));
-
-document.querySelectorAll('link[rel="stylesheet"]')
-    .forEach(el => el.parentNode.removeChild(el));
-
-document.querySelectorAll('style')
-    .forEach(el => el.parentNode.removeChild(el));
